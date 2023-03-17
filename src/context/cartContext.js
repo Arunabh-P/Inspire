89361f10404f950d27881fcc60a0c99aec381758
Arunabh-P <arunabh1995@gmail.com>
2023-03-17 14:10:20 +0530

    final change

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -1,43 +1,46 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
-import reducer from "../reducer/cartReducer";
+import { createContext, useContext, useEffect, useReducer } from 'react';
+import reducer from '../reducer/cartReducer';
 
 const CartContext = createContext();
 
 const getLocalCartData = () => {
-  let localCartData = localStorage.getItem("inspireCart");
-  if (!localCartData) {
-    return [];
-  } else {
-    return JSON.parse(localCartData);
-  }
+  let localCartData = localStorage.getItem('inspireCart');
+  // if (!localCartData) {
+  //   return [];
+  // } else {
+  //   return JSON.parse(localCartData);
+  // }
+  const parseData = JSON.parse(localCartData);
+  if (!Array.isArray(parseData)) return [];
+  return parseData;
 };
 const initialState = {
   cart: getLocalCartData(),
-  total_item: "",
-  total_amount: "",
+  total_item: '',
+  total_amount: '',
   shipping_fee: 50000,
 };
 const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const addToCart = (id, color, amount, product) => {
-    dispatch({ type: "ADD_TO_CART", payload: { id, color, amount, product } });
+    dispatch({ type: 'ADD_TO_CART', payload: { id, color, amount, product } });
   };
   // increment and decrement product
   const setDecrement = (id) => {
-    dispatch({ type: "SET_DECREMENT", payload: id });
+    dispatch({ type: 'SET_DECREMENT', payload: id });
   };
   const setIncrement = (id) => {
-    dispatch({ type: "SET_INCREMENT", payload: id });
+    dispatch({ type: 'SET_INCREMENT', payload: id });
   };
 
   const removeItem = (id) => {
-    dispatch({ type: "REMOVE_ITEM", payload: id });
+    dispatch({ type: 'REMOVE_ITEM', payload: id });
   };
 
   // clear cart
   const clearCart = () => {
-    dispatch({ type: "CLEAR_CART" });
+    dispatch({ type: 'CLEAR_CART' });
   };
 
   useEffect(() => {
@@ -45,10 +48,10 @@ const CartProvider = ({ children }) => {
     // dispatch({ type: 'CART_TOTAL_PRICE' });
 
     // simplifiying code above
-    dispatch({ type: "CART_ITEM_PRICE_TOTAL" });
+    dispatch({ type: 'CART_ITEM_PRICE_TOTAL' });
 
     //  add data to localStorage
-    localStorage.setItem("inspireCart", JSON.stringify(state.cart));
+    localStorage.setItem('inspireCart', JSON.stringify(state.cart));
   }, [state.cart]);
 
   return (
